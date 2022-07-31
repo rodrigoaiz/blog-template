8ca8b16adb39e55cf55632f1481afeb9559469ee
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2022-07-30 23:01:53 -0500

    correcion

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@ function rodrigoaiz_register_styles(){
 add_action('wp_enqueue_scripts', 'rodrigoaiz_register_styles');
 
 function rodrigoaiz_register_scripts(){
-    wp_enqueue_style('rodrigoaiz-script', get_template_directory_uri() . "/main.js", array(), '1.0', true);
-    wp_enqueue_style('rodrigoaiz-bootstrapjs', "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js", array(), '5.2.0', true);
-    wp_enqueue_style('rodrigoaiz-jquery', "https://code.jquery.com/jquery-3.6.0.slim.min.js", array(), '3.6.0', true);
+    wp_enqueue_script('rodrigoaiz-script', get_template_directory_uri() . "/main.js", array(), '1.0', true);
+    wp_enqueue_script('rodrigoaiz-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js", array(), '5.2.0', true);
+    wp_enqueue_script('rodrigoaiz-jquery', "https://code.jquery.com/jquery-3.6.0.slim.min.js", array(), '3.6.0', true);
 }
 add_action( 'wp_enqueue_scripts' , 'rodrigoaiz_register_scripts' );
 ?>
