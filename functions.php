c7276199e8ff82fc31794a77d98d249d84ad25a1
rodrigoaiz <rodrigoaiz@gmail.com>
2022-07-30 23:17:06 -0500

    add directorios

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function rodrigoaiz_register_styles(){
 add_action('wp_enqueue_scripts', 'rodrigoaiz_register_styles');
 
 function rodrigoaiz_register_scripts(){
-    wp_enqueue_script('rodrigoaiz-script', get_template_directory_uri() . "/main.js", array(), '1.0', true);
+    wp_enqueue_script('rodrigoaiz-script', get_template_directory_uri() . "./assets/js/main.js", array(), '1.0', true);
     wp_enqueue_script('rodrigoaiz-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js", array(), '5.2.0', true);
     wp_enqueue_script('rodrigoaiz-jquery', "https://code.jquery.com/jquery-3.6.0.slim.min.js", array(), '3.6.0', true);
 }
